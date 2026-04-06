417e19d4eb9f763040c2c3d2c3ebc5efc5d7d2c9
Antonio Correa <antonio.correa@gmail.com>
2026-04-06 18:34:40 -0400

    fix: import CSS in client entry point

--- apps/web/app/client.tsx
+++ apps/web/app/client.tsx
@@ -1,6 +1,7 @@
 import { StartClient } from '@tanstack/react-start/client';
 import { hydrateRoot } from 'react-dom/client';
 import { createRouter } from './router.js';
+import './styles/app.css';
 
 const router = createRouter();
 
